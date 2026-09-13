dbf387aecd8dc8cc3c05685da27e3c6d7aa2d0df
Steven Kaufman <steven.charles.kaufman@gmail.com>
2026-09-12 19:31:45 -0500

    rename serializers to deserializers

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,9 +1,9 @@
+pub mod deserializers;
 pub mod errors;
 pub mod schema;
-pub mod serializers;
 pub mod tuple;
 
+pub use deserializers::*;
 pub use errors::*;
 pub use schema::*;
-pub use serializers::*;
 pub use tuple::*;
