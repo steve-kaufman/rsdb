b8f4d6c910c4ebe60ca5f22f2c0fbc6ff91fb191
Steven Kaufman <steven.charles.kaufman@gmail.com>
2026-09-13 18:38:02 -0500

    reorganize

--- src/db/deserializers.rs
+++ src/db/deserializers.rs
@@ -16,7 +16,7 @@ pub fn is_null(reader: &mut impl Read) -> Result<bool, Error> {
     }
 }
 
-pub fn parse_int(reader: &mut impl Read) -> Result<i64, Error> {
+pub fn deserialize_int(reader: &mut impl Read) -> Result<i64, Error> {
     let mut buf: [u8; 8] = [0; 8];
     reader
         .read_exact(&mut buf)
@@ -24,7 +24,7 @@ pub fn parse_int(reader: &mut impl Read) -> Result<i64, Error> {
     Ok(i64::from_le_bytes(buf))
 }
 
-pub fn parse_float(reader: &mut impl Read) -> Result<f64, Error> {
+pub fn deserialize_float(reader: &mut impl Read) -> Result<f64, Error> {
     let mut buf: [u8; 8] = [0; 8];
     reader
         .read_exact(&mut buf)
@@ -32,7 +32,7 @@ pub fn parse_float(reader: &mut impl Read) -> Result<f64, Error> {
     Ok(f64::from_le_bytes(buf))
 }
 
-pub fn parse_text(reader: &mut impl Read) -> Result<String, Error> {
+pub fn deserialize_text(reader: &mut impl Read) -> Result<String, Error> {
     let mut len_buf: [u8; 2] = [0; 2];
     reader
         .read_exact(&mut len_buf)
@@ -52,7 +52,7 @@ pub fn parse_text(reader: &mut impl Read) -> Result<String, Error> {
     String::from_utf8(text_buf).map_err(|e| Error::InvalidUtf8(e.to_string()))
 }
 
-pub fn parse_uuid(reader: &mut impl Read) -> Result<Uuid, Error> {
+pub fn deserialize_uuid(reader: &mut impl Read) -> Result<Uuid, Error> {
     let mut buf: [u8; 16] = [0; 16];
     reader
         .read_exact(&mut buf)
@@ -95,102 +95,114 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_int() {
+    fn test_deserialize_int() {
         // Needs 8 bytes
-        assert!(is_io_error(parse_int(&mut Cursor::new(&[1])).unwrap_err()));
         assert!(is_io_error(
-            parse_int(&mut Cursor::new(&[1, 2, 3, 4, 5, 6, 7])).unwrap_err()
+            deserialize_int(&mut Cursor::new(&[1])).unwrap_err()
+        ));
+        assert!(is_io_error(
+            deserialize_int(&mut Cursor::new(&[1, 2, 3, 4, 5, 6, 7])).unwrap_err()
         ));
 
         // Exactly 8 bytes
-        assert_eq!(parse_int(&mut Cursor::new(0i64.to_le_bytes())), Ok(0));
+        assert_eq!(deserialize_int(&mut Cursor::new(0i64.to_le_bytes())), Ok(0));
         assert_eq!(
-            parse_int(&mut Cursor::new(i64::MIN.to_le_bytes())),
+            deserialize_int(&mut Cursor::new(i64::MIN.to_le_bytes())),
             Ok(i64::MIN)
         );
         assert_eq!(
-            parse_int(&mut Cursor::new(i64::MAX.to_le_bytes())),
+            deserialize_int(&mut Cursor::new(i64::MAX.to_le_bytes())),
             Ok(i64::MAX)
         );
         assert_eq!(
-            parse_int(&mut Cursor::new(12345i64.to_le_bytes())),
+            deserialize_int(&mut Cursor::new(12345i64.to_le_bytes())),
             Ok(12345)
         );
 
         // More than 8 bytes
         assert_eq!(
-            parse_int(&mut Cursor::new(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])),
+            deserialize_int(&mut Cursor::new(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])),
             Ok(i64::from_le_bytes([1, 2, 3, 4, 5, 6, 7, 8]))
         );
     }
 
     #[test]
-    fn test_parse_float() {
+    fn test_deserialize_float() {
         // Needs 8 bytes
         assert!(is_io_error(
-            parse_float(&mut Cursor::new(&[1])).unwrap_err()
+            deserialize_float(&mut Cursor::new(&[1])).unwrap_err()
         ));
         assert!(is_io_error(
-            parse_float(&mut Cursor::new(&[1, 2, 3, 4, 5, 6, 7])).unwrap_err()
+            deserialize_float(&mut Cursor::new(&[1, 2, 3, 4, 5, 6, 7])).unwrap_err()
         ));
 
         // Exactly 8 bytes
-        assert_eq!(parse_float(&mut Cursor::new(0f64.to_le_bytes())), Ok(0.));
         assert_eq!(
-            parse_float(&mut Cursor::new(f64::MIN.to_le_bytes())),
+            deserialize_float(&mut Cursor::new(0f64.to_le_bytes())),
+            Ok(0.)
+        );
+        assert_eq!(
+            deserialize_float(&mut Cursor::new(f64::MIN.to_le_bytes())),
             Ok(f64::MIN)
         );
         assert_eq!(
-            parse_float(&mut Cursor::new(f64::MAX.to_le_bytes())),
+            deserialize_float(&mut Cursor::new(f64::MAX.to_le_bytes())),
             Ok(f64::MAX)
         );
         assert_eq!(
-            parse_float(&mut Cursor::new(12345.0f64.to_le_bytes())),
+            deserialize_float(&mut Cursor::new(12345.0f64.to_le_bytes())),
             Ok(12345.)
         );
 
         // More than 8 bytes
         assert_eq!(
-            parse_float(&mut Cursor::new(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])),
+            deserialize_float(&mut Cursor::new(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])),
             Ok(f64::from_le_bytes([1, 2, 3, 4, 5, 6, 7, 8]))
         );
     }
 
     #[test]
-    fn test_parse_text() {
+    fn test_deserialize_text() {
         // Needs at least 2 bytes
-        assert!(is_io_error(parse_text(&mut Cursor::new(&[])).unwrap_err()));
-        assert!(is_io_error(parse_text(&mut Cursor::new(&[1])).unwrap_err()));
+        assert!(is_io_error(
+            deserialize_text(&mut Cursor::new(&[])).unwrap_err()
+        ));
+        assert!(is_io_error(
+            deserialize_text(&mut Cursor::new(&[1])).unwrap_err()
+        ));
 
         // len 0 returns ""
-        assert_eq!(parse_text(&mut Cursor::new(&[0, 0])), Ok("".to_string()));
         assert_eq!(
-            parse_text(&mut Cursor::new(&[0, 0, 1, 2, 3])),
+            deserialize_text(&mut Cursor::new(&[0, 0])),
+            Ok("".to_string())
+        );
+        assert_eq!(
+            deserialize_text(&mut Cursor::new(&[0, 0, 1, 2, 3])),
             Ok("".to_string())
         );
 
         // len 'n' requires 'n' additional bytes
         assert_eq!(
-            parse_text(&mut Cursor::new(1i16.to_le_bytes())),
+            deserialize_text(&mut Cursor::new(1i16.to_le_bytes())),
             Err(Error::NotEnoughTextBytes {
                 len: 1,
                 bytes_read: 0
             })
         );
         assert_eq!(
-            parse_text(&mut Cursor::new(
+            deserialize_text(&mut Cursor::new(
                 [&1i16.to_le_bytes(), "a".as_bytes()].concat()
             )),
             Ok("a".to_string())
         );
         assert_eq!(
-            parse_text(&mut Cursor::new(
+            deserialize_text(&mut Cursor::new(
                 [&1i16.to_le_bytes(), "abcd".as_bytes()].concat()
             )),
             Ok("a".to_string())
         );
         assert_eq!(
-            parse_text(&mut Cursor::new(
+            deserialize_text(&mut Cursor::new(
                 [&5i16.to_le_bytes(), "abcd".as_bytes()].concat()
             )),
             Err(Error::NotEnoughTextBytes {
@@ -199,13 +211,13 @@ mod tests {
             })
         );
         assert_eq!(
-            parse_text(&mut Cursor::new(
+            deserialize_text(&mut Cursor::new(
                 [&5i16.to_le_bytes(), "abcde".as_bytes()].concat()
             )),
             Ok("abcde".to_string())
         );
         assert_eq!(
-            parse_text(&mut Cursor::new(
+            deserialize_text(&mut Cursor::new(
                 [&5i16.to_le_bytes(), "abcdefghi".as_bytes()].concat()
             )),
             Ok("abcde".to_string())
@@ -213,11 +225,13 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_uuid() {
+    fn test_deserialize_uuid() {
         // Needs 16 bytes
-        assert!(is_io_error(parse_uuid(&mut Cursor::new(&[])).unwrap_err()));
         assert!(is_io_error(
-            parse_uuid(&mut Cursor::new(&[
+            deserialize_uuid(&mut Cursor::new(&[])).unwrap_err()
+        ));
+        assert!(is_io_error(
+            deserialize_uuid(&mut Cursor::new(&[
                 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
             ]))
             .unwrap_err()
@@ -225,10 +239,13 @@ mod tests {
 
         // Correctly parses uuid
         let uuid1 = Uuid::new_v4();
-        assert_eq!(parse_uuid(&mut Cursor::new(uuid1.to_bytes_le())), Ok(uuid1));
+        assert_eq!(
+            deserialize_uuid(&mut Cursor::new(uuid1.to_bytes_le())),
+            Ok(uuid1)
+        );
         let uuid2 = Uuid::new_v4();
         assert_eq!(
-            parse_uuid(&mut Cursor::new(
+            deserialize_uuid(&mut Cursor::new(
                 [uuid2.to_bytes_le().as_slice(), &[1, 2, 3]].concat()
             )),
             Ok(uuid2)
--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,9 +1,11 @@
 pub mod deserializers;
 pub mod errors;
 pub mod schema;
+pub mod serializers;
 pub mod tuple;
 
 pub use deserializers::*;
 pub use errors::*;
 pub use schema::*;
+pub use serializers::*;
 pub use tuple::*;
