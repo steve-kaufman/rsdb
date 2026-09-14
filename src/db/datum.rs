304f7b7fa900fb3b0d874b8f3d6a2c68b13d313d
Steven Kaufman <steven.charles.kaufman@gmail.com>
2026-09-13 22:39:30 -0500

    add deserialize_datum

--- src/db/datum.rs
+++ src/db/datum.rs
@@ -1,11 +1,12 @@
-use std::io::Write;
+use std::io::{Read, Write};
 
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 use crate::db::{
-    ColumnDefinition, DataType, Error, serialize_float, serialize_int, serialize_null,
-    serialize_text, serialize_uuid,
+    ColumnDefinition, DataType, Error, deserialize_float, deserialize_int, deserialize_text,
+    deserialize_uuid, is_null, serialize_float, serialize_int, serialize_null, serialize_text,
+    serialize_uuid,
 };
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -68,8 +69,25 @@ pub fn serialize_null_datum(buf: &mut impl Write, col: &ColumnDefinition) -> Res
     }
 }
 
+pub fn deserialize_datum(
+    buf: &mut impl Read,
+    col: &ColumnDefinition,
+) -> Result<Option<Datum>, Error> {
+    if col.nullable && is_null(buf)? {
+        return Ok(None);
+    }
+    match col.data_type {
+        DataType::Integer => Ok(Some(Datum::Integer(deserialize_int(buf)?))),
+        DataType::Float => Ok(Some(Datum::Float(deserialize_float(buf)?))),
+        DataType::Text => Ok(Some(Datum::Text(deserialize_text(buf)?))),
+        DataType::Uuid => Ok(Some(Datum::Uuid(deserialize_uuid(buf)?))),
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use std::io::Cursor;
+
     use super::*;
 
     #[test]
@@ -336,5 +354,155 @@ mod tests {
         serialize_uuid(&mut expected, id).unwrap();
 
         assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_deserialize_propagates_io_errors() {
+        let buf = vec![];
+
+        let col = ColumnDefinition {
+            name: "id".to_string(),
+            nullable: true,
+            data_type: DataType::Integer,
+        };
+
+        let res = deserialize_datum(&mut Cursor::new(buf), &col);
+
+        assert!(matches!(res, Err(Error::IO(_))));
+    }
+
+    #[test]
+    fn test_deserialize_null() {
+        for data_type in [
+            DataType::Float,
+            DataType::Integer,
+            DataType::Uuid,
+            DataType::Text,
+        ] {
+            let mut buf = vec![];
+            serialize_null(&mut buf, true).unwrap();
+            let col = ColumnDefinition {
+                name: "id".to_string(),
+                nullable: true,
+                data_type,
+            };
+
+            let res = deserialize_datum(&mut Cursor::new(buf), &col);
+            assert_eq!(res, Ok(None))
+        }
+    }
+
+    #[test]
+    fn test_deserialize_integer() {
+        // Nullable
+        let mut buf = vec![];
+        serialize_null(&mut buf, false).unwrap();
+        serialize_int(&mut buf, 123).unwrap();
+        let col = ColumnDefinition {
+            name: "id".to_string(),
+            nullable: true,
+            data_type: DataType::Integer,
+        };
+
+        let res = deserialize_datum(&mut Cursor::new(buf), &col);
+        assert_eq!(res, Ok(Some(Datum::Integer(123))));
+
+        // Non-Nullable
+        let mut buf = vec![];
+        serialize_int(&mut buf, 123).unwrap();
+        let col = ColumnDefinition {
+            name: "id".to_string(),
+            nullable: false,
+            data_type: DataType::Integer,
+        };
+
+        let res = deserialize_datum(&mut Cursor::new(buf), &col);
+        assert_eq!(res, Ok(Some(Datum::Integer(123))));
+    }
+
+    #[test]
+    fn test_deserialize_float() {
+        // Nullable
+        let mut buf = vec![];
+        serialize_null(&mut buf, false).unwrap();
+        serialize_float(&mut buf, 123.456).unwrap();
+        let col = ColumnDefinition {
+            name: "id".to_string(),
+            nullable: true,
+            data_type: DataType::Float,
+        };
+
+        let res = deserialize_datum(&mut Cursor::new(buf), &col);
+        assert_eq!(res, Ok(Some(Datum::Float(123.456))));
+
+        // Non-Nullable
+        let mut buf = vec![];
+        serialize_float(&mut buf, 123.456).unwrap();
+        let col = ColumnDefinition {
+            name: "id".to_string(),
+            nullable: false,
+            data_type: DataType::Float,
+        };
+
+        let res = deserialize_datum(&mut Cursor::new(buf), &col);
+        assert_eq!(res, Ok(Some(Datum::Float(123.456))));
+    }
+
+    #[test]
+    fn test_deserialize_text() {
+        // Nullable
+        let mut buf = vec![];
+        serialize_null(&mut buf, false).unwrap();
+        serialize_text(&mut buf, "hello").unwrap();
+        let col = ColumnDefinition {
+            name: "id".to_string(),
+            nullable: true,
+            data_type: DataType::Text,
+        };
+
+        let res = deserialize_datum(&mut Cursor::new(buf), &col);
+        assert_eq!(res, Ok(Some(Datum::Text("hello".to_string()))));
+
+        // Non-Nullable
+        let mut buf = vec![];
+        serialize_text(&mut buf, "hello").unwrap();
+        let col = ColumnDefinition {
+            name: "id".to_string(),
+            nullable: false,
+            data_type: DataType::Text,
+        };
+
+        let res = deserialize_datum(&mut Cursor::new(buf), &col);
+        assert_eq!(res, Ok(Some(Datum::Text("hello".to_string()))));
+    }
+
+    #[test]
+    fn test_deserialize_uuid() {
+        // Nullable
+        let mut buf = vec![];
+        let id = Uuid::new_v4();
+        serialize_null(&mut buf, false).unwrap();
+        serialize_uuid(&mut buf, id).unwrap();
+        let col = ColumnDefinition {
+            name: "id".to_string(),
+            nullable: true,
+            data_type: DataType::Uuid,
+        };
+
+        let res = deserialize_datum(&mut Cursor::new(buf), &col);
+        assert_eq!(res, Ok(Some(Datum::Uuid(id))));
+
+        // Non-Nullable
+        let mut buf = vec![];
+        let id = Uuid::new_v4();
+        serialize_uuid(&mut buf, id).unwrap();
+        let col = ColumnDefinition {
+            name: "id".to_string(),
+            nullable: false,
+            data_type: DataType::Uuid,
+        };
+
+        let res = deserialize_datum(&mut Cursor::new(buf), &col);
+        assert_eq!(res, Ok(Some(Datum::Uuid(id))));
     }
 }
