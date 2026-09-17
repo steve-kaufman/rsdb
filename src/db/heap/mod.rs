1c05c98409137d618a280689d75051bfa4911b16
Steven Kaufman <steven.charles.kaufman@gmail.com>
2026-09-17 16:36:23 -0500

    reorganize

---
+++ src/db/heap/mod.rs
@@ -0,0 +1,7 @@
+pub mod heapcache;
+pub mod page_id;
+pub mod pagedfile;
+
+pub use heapcache::*;
+pub use page_id::*;
+pub use pagedfile::*;
--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,17 +1,13 @@
 pub mod constants;
 pub mod errors;
-pub mod heapcache;
-pub mod index;
-pub mod page_id;
-pub mod pagedfile;
+pub mod heap;
+pub mod indexes;
 pub mod schema;
 pub mod tuples;
 
 pub use constants::*;
 pub use errors::*;
-pub use heapcache::*;
-pub use index::*;
-pub use page_id::*;
-pub use pagedfile::*;
+pub use heap::*;
+pub use indexes::*;
 pub use schema::*;
 pub use tuples::*;
